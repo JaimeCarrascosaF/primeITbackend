b6040eb75453684faa4136aa48179b00e280097f
Jaime Carrascosa <inf.jaime1@gmail.com>
2024-01-02 16:19:52 +0100

    Enabling CORS for server

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import apiRouter from '../routes/routes';
 import errorHandler, { defaultErrorHandler } from '../controllers/errorController';
 
+const cors = require('cors');
+
 const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/api', apiRouter);
 app.use(errorHandler);
 app.use(defaultErrorHandler);
